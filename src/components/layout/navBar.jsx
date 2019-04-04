dd8d70f9d9820962290cbed9add48288d483ac4e
LiweniMac <554052409@qq.com>
2019-04-04 21:12:02 +1100

    update nav bar

--- src/components/layout/navBar.jsx
+++ src/components/layout/navBar.jsx
@@ -1,51 +1,57 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
         <div className="container">
-          <Link className="navbar-brand" to="/">
-            <h1>
-              <span className="text-info">T</span>-Plan
-            </h1>
-          </Link>
+          <NavLink className="navbar-brand" to="/">
+            <div
+              className="text-center
+            "
+            >
+              <h1>
+                <span className="text-info p-1">T</span>-Plan
+              </h1>
+            </div>
+          </NavLink>
+
           <button
             className="navbar-toggler"
             type="button"
             data-toggle="collapse"
             data-target="#tplan-nav"
+            aria-expanded="false"
           >
             <span className="navbar-toggler-icon" />
           </button>
 
           <div className="collapse navbar-collapse" id="tplan-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/rightsQuiz">
-                  Rights Quiz
-                </Link>
-              </li>
+            <div className="navbar-nav ml-auto">
+              <NavLink className="nav-link nav-item" to="/rightsQuiz">
+                <div className="text-center">
+                  <p className="navTitle">Rights Quiz </p>
+                </div>
+              </NavLink>
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
+              <NavLink className="nav-link nav-item" to="/occupations">
+                <div className="text-center">
+                  <p className="navTitle">Occupations </p>
+                </div>
+              </NavLink>
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-            </ul>
+              <NavLink className="nav-link nav-item" to="/courses">
+                <div className="text-center">
+                  <p className="navTitle">Course</p>
+                </div>
+              </NavLink>
+              <NavLink className="nav-link nav-item" to="/housing">
+                <div className="text-center">
+                  <p className="navTitle">Housing</p>
+                </div>
+              </NavLink>
+            </div>
           </div>
         </div>
       </nav>
